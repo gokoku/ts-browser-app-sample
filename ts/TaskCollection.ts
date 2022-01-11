bf5fe92cb6413a16e4a67e75969ccf49402f8636
gokoku <george.6809@gmail.com>
2022-01-11 13:11:44 +0900

    ストレージに保存、しかしstatusはtodoのまま

--- ts/TaskCollection.ts
+++ ts/TaskCollection.ts
@@ -1,14 +1,23 @@
-import { Status, Task } from './Task'
+import { Status, Task, TaskObject } from './Task'
 
+const STORAGE_KEY = 'TASKS'
 export class TaskCollection {
+  private readonly storage
   private tasks: Task[] = []
+
+  constructor() {
+    this.storage = localStorage
+    this.tasks = this.getStoredTasks()
+  }
 
   add(task: Task) {
     this.tasks.push(task)
+    this.updateStorage()
   }
 
   delete(task: Task) {
     this.tasks = this.tasks.filter(t => t.id !== task.id)
+    this.updateStorage()
   }
 
   find(id: string): Task | null {
@@ -28,4 +37,29 @@ export class TaskCollection {
     return this.tasks.filter(({ status }) => status === filterStatus)
   }
 
+  private updateStorage() {
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks))
+  }
+
+  private getStoredTasks(): Task[] {
+    const jsonString = this.storage.getItem(STORAGE_KEY)
+    if (!jsonString) return []
+    try{
+      const storedTasks = JSON.parse(jsonString)
+
+      assertIsTaskObjects(storedTasks)
+
+      const tasks = storedTasks.map(t => new Task(t))
+      return tasks
+    } catch (e) {
+      this.storage.removeItem(STORAGE_KEY)
+      return []
+    }
+  }
+}
+
+function assertIsTaskObjects(value: any): asserts value is TaskObject[] {
+  if(!Array.isArray(value) || !value.every((item) => Task.validate(item))) {
+    throw new Error('引数「value」は TaskObject[] 型と一致しません。')
+  }
 }
--- ts/TaskRenderer.ts
+++ ts/TaskRenderer.ts
@@ -1,6 +1,7 @@
 import dragula from 'dragula'
 
 import { Status, Task, statusMap } from './Task'
+import { TaskCollection } from './TaskCollection'
 
 export class TaskRenderer {
   constructor(private readonly todoList: HTMLElement,
@@ -13,6 +14,14 @@ export class TaskRenderer {
 
     this.todoList.append(taskEl)
     return { deleteButtonEl }
+  }
+
+  renderAll(taskCollection: TaskCollection) {
+    const todoTasks = this.renderList(taskCollection.filter(statusMap.todo), this.todoList)
+    const doingTasks = this.renderList(taskCollection.filter(statusMap.doing), this.doingList)
+    const doneTasks = this.renderList(taskCollection.filter(statusMap.done), this.doneList)
+
+    return [...todoTasks, ...doingTasks, ...doneTasks]
   }
 
   private render(task: Task) {
@@ -32,6 +41,23 @@ export class TaskRenderer {
     taskEl.append(spanEl, deleteButtonEl)
 
     return {taskEl, deleteButtonEl}
+  }
+
+  private renderList(tasks: Task[], listEl: HTMLElement) {
+    if(tasks.length === 0) return[]
+    const taskList: Array<{
+      task: Task,
+      deleteButtonEl: HTMLButtonElement
+    }> = []
+
+    tasks.forEach((task) => {
+      const {taskEl, deleteButtonEl} = this.render(task)
+
+      listEl.append(taskEl)
+      taskList.push({task, deleteButtonEl})
+    })
+
+    return taskList
   }
 
   remove(task: Task) {
